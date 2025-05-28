ddf9091c4fdd4d4d8daa340d29f5939eed4ce5ba
John Biddiscombe <biddisco@cscs.ch>
2025-05-28 22:32:03 +0200

    Disable fortran until compilation problems fixed

--- site/repo/packages/openmpi/package.py
+++ site/repo/packages/openmpi/package.py
@@ -1030,7 +1030,7 @@ with '-Wl,-commons,use_dylibs' and without
 
     def configure_args(self):
         spec = self.spec
-        config_args = ["--enable-shared", "--disable-silent-rules", "--disable-sphinx"]
+        config_args = ["--enable-shared", "--disable-silent-rules", "--disable-sphinx", "--enable-mpi-fortran=no"]
 
         # Work around incompatibility with new apple-clang linker
         # https://github.com/open-mpi/ompi/issues/12427
