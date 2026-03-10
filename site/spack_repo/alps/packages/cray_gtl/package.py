10901c63af489e45789ed4854ed2b593f1880a55
Simon Pintarelli <1237199+simonpintarelli@users.noreply.github.com>
2026-03-10 17:31:47 +0100

    run patchelf when depending on rocm 7 (#79)
    Currently all cray-mpich versions are explicitly linked to
    `libamdhip64.so.6`. In case `rocm@7:` is in the dependencies, run
    `patchelf --replace-needed` to link to `libamdhip64.so.7` instead.
    
    ---------
    
    Co-authored-by: Mikael Simberg <mikael.simberg@iki.fi>

--- site/spack_repo/alps/packages/cray_gtl/package.py
+++ site/spack_repo/alps/packages/cray_gtl/package.py
@@ -141,6 +141,8 @@ class CrayGtl(Package):
                 if "@8.1.27+cuda" in self.spec:
                     patchelf("--add-needed", "libcudart.so", f, fail_on_error=False)
                     patchelf("--add-needed", "libcuda.so", f, fail_on_error=False)
+                if "%hip@7:" in self.spec:
+                    patchelf("--replace-needed", "libamdhip64.so.6", "libamdhip64.so.7", f, fail_on_error=False)
 
     @run_after("install")
     def fixup_pkgconfig(self):
