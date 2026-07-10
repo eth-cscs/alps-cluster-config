cdfc32deb52887eb0932c4f878d87dd73d92924d
bcumming <bcumming@cscs.ch>
2026-07-10 17:01:51 +0200

    add nvhpc spack package for v26.5

--- site/spack_repo/alps/packages/nvhpc/package.py
+++ site/spack_repo/alps/packages/nvhpc/package.py
@@ -23,6 +23,16 @@ from spack.package import *
 #  - package key must be in the form '{os}-{arch}' where 'os' is in the
 #    format returned by platform.system() and 'arch' by platform.machine()
 _versions = {
+    "26.5": {
+        "Linux-aarch64": (
+            "4a7c8fd4d429f5d04634163d7c40840a8802aac57a9357f9a1f9b1cdb5203de8",
+            "https://developer.download.nvidia.com/hpc-sdk/26.5/nvhpc_2026_265_Linux_aarch64_cuda_13.2.tar.gz",
+        ),
+        "Linux-x86_64": (
+            "8849b3909f7e75df4c32af4dc585bf0701d85495da43221b090a9af77df203b6"
+            "https://developer.download.nvidia.com/hpc-sdk/26.5/nvhpc_2026_265_Linux_x86_64_cuda_13.2.tar.gz",
+        ),
+    },
     "26.3": {
         "Linux-aarch64": (
             "b7cb60c89e149e2695bc69cdee79270922d681689cdf41c40217ddd15231cdc9",
